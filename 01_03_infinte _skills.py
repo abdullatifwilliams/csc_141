b201f694b5608c3ec7b6aabef648d8caad3195d3
abdullatifwilliams <williamsabdul27@gmail.com>
2024-09-02 09:13:38 -0400

    Update 01_03_infinte _skills.py

--- 01_03_infinte _skills.py
+++ 01_03_infinte _skills.py
@@ -1,3 +1,6 @@
+#infinte skills
 # i would build a software that can detact cancer even when its not fully devloped 
 # Also a gaming program such as a rp based 
 # also a program that can create photoshop pictures 
+
+ 
