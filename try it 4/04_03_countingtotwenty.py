15207d191bedb7c46c927e0158349ec300fd5320
abdullatif-williams <williamsabdul27@gmail.com>
2024-09-20 00:22:13 -0400

    Add files via upload

---
+++ try it 4/04_03_countingtotwenty.py
@@ -0,0 +1,3 @@
+countings=('1,2,3,4,5,6,7,8,9,,10,11,12,13,14,15,16,17,18,19,20')
+for counting in countings(1,20):
+    print(counting)
---
+++ try it 4/04_12_moreloops.py
@@ -0,0 +1,11 @@
+fruits = ['Apple', 'Banana', 'Cherry', 'Mango', 'Orange']
+
+vegetables = ['Carrot', 'Broccoli', 'Spinach', 'Pepper', 'Tomato']
+
+print("My favorite fruits are:")
+for fruit in fruits:
+    print("-", fruit)
+
+print("\nMy favorite vegetables are:")
+for vegetable in vegetables:
+    print("-", vegetable)
